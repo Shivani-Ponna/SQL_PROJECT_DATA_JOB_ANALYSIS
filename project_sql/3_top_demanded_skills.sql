89dc6c42936adc244b780001e37242ede0b49e95
shivani ponna <shivaniponna23@gmail.com>
2026-08-07 16:06:34 -0300

    Add top demanded skills query

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -6,7 +6,7 @@ FROM
 INNER  JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id 
 WHERE
-    job_title_short = 'Data Analyst' 
+    job_title_short = 'Data Analyst' AND job_country = 'Canada'
 GROUP BY
     skills
 ORDER BY
